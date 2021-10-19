a3ac9583f5aa339959ebfde90d0d916f4f129abe
Roberto Roman <91975410+rroman5@users.noreply.github.com>
2021-10-19 15:35:24 -0500

    Group project

---
+++ group project.py
@@ -0,0 +1,44 @@
+print ("Please choose your option from the list below: ")
+learn_py = 1
+learn_java = 2
+go_swimming = 3
+have_dinner = 4
+bed = 5
+exit = 0
+
+
+print ("1.  Learn python")
+print ("2.  Learn Java")
+print ("3.  Go swimming")
+print ("4.  Have dinner")
+print ("5.  Go to bed")
+print ("0.  Exit")
+
+command = int(input("Enter command"))
+if command == 1:
+    print ("Please don't learn python, its complicated ._.")
+
+if command==2:
+    print ("Java is cool")
+
+if command==3:
+    print ("Swimming is fun, but keep an eye out of sharks >.>")
+
+if command==4:
+    print ("ahhh the best part when hungry d:")
+
+if command==5:
+    print ("Sleeping is my favorite things out of the day Zzzzzz")
+
+if command==0:
+   print ("Exit please")
+    
+
+
+
+
+       
+
+
+
+
